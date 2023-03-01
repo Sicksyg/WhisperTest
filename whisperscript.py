68e25a9f60881da315a75d60dfcefa8ae9c0a05f
Kristian <krille.sick@gmail.com>
2023-03-01 23:27:34 +0100

    add json

--- whisperscript.py
+++ whisperscript.py
@@ -2,6 +2,8 @@ import whisper
 import argparse
 from timeit import default_timer
 import torch
+import json
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -11,6 +13,7 @@ def main():
     parser.add_argument("-m", "--modelsize", type=str, default="base",
                     help="Specify the model, can be: base, small, medium, large")
     parser.add_argument("-o", "--output", type=str, help="Define an output path for transcribed audio file")
+    parser.add_argument("-json", type=bool, default=False, help="Output as a json file")
     parser.add_argument("-d", "--device", type=str, default="cpu", help="Specify use of GPU (option: cuda) or CPU (option cpu)")
     args = parser.parse_args()
     
@@ -25,25 +28,33 @@ def loadModel(args):
         if tmem < 10000:
             print(f"Not enough vram at GPU to load the large model. GPU vram: {tmem} MB. Need 10000 MB ´(10GB) to run. Setting device to CPU")
         
-            
-
-
     model = whisper.load_model(args.modelsize)
-    print(f"Model is loaded, modelsize= {args.modelsize} Time: {round(default_timer() - timeLoadModel)} s. \n")
+    print(f"Model is loaded - modelsize = {args.modelsize} - Time to load: {round(default_timer() - timeLoadModel)} s. \n")
     return model
 
 def transcribeAudio(args, model):
+    print("Starting transcription")
     transcribeTime = default_timer()
-    result = model.transcribe(args.filename)
-    print(f"Files has been transcribet. Time: {round(default_timer() - transcribeTime)} s. \n")
+    result = model.transcribe(args.filename, verbose= True)
+    print(f"Files has been transcribet. - Time: {round(default_timer() - transcribeTime)} s. \n")
+
 
     if args.output:
-        with open(args.output, "w", encoding="utf-8") as fp:
-            fp.write(result["text"])
-            print(f"Output saved to {args.output}")   
+        if args.json:
+            outputjson = args.filename[:-4] + ".json"
+            with open(outputjson, "w", encoding="utf-8") as fp:
+                json.dump(result["segments"], fp, indent=4)
+            print(f"Output saved to {outputjson}")   
+        else:
+            outputtxt = args.filename[:-4] + ".json"
+            with open(outputtxt, "w", encoding="utf-8") as fp:
+                fp.write(result["text"])
+                print(f"Output saved to {outputtxt}")   
     else:
         print("Transcribed text \n")
-        print(result["text"])
+        print(result["segments"])
+
+
 
 if __name__ == '__main__':
     main()
